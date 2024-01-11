a314bc3228c3155827437ecafa17ec217a9efa78
muntaqam <maahi305@yahoo.com>
2024-01-11 17:59:33 -0500

    trying to fix the faicon resizing issue, works in local host

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import "../../styles/globals.css";
 
-
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store/store";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
