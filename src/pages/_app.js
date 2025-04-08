290b807cade6d876fdbb86bf89479afdc0f7d10d
muntaqam <maahi305@yahoo.com>
2025-04-08 16:55:31 -0400

    added GA tracking scripts in _app.js to monitor page views and user behavior

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,18 +5,40 @@ import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store/store";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Script from "next/script"; // ✅ Required for GA
 
-// Prevent fontawesome from dynamically adding its css since we did it manually above
 config.autoAddCss = false;
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ReduxProvider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </ReduxProvider>
+    <>
+      {/* ✅ Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1WN57SLH26"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1WN57SLH26', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <ReduxProvider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ReduxProvider>
+    </>
   );
 }
 
